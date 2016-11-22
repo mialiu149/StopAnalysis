b6b4ccfdf3fc90bd5c5cc3e8118af1ae878dfc32
Mia Liu <miaoyuan.liu0@gmail.com>
2016-11-22 07:13:20 -0800

    useless readme

---
+++ onelepbabymaker/runBabyMakerCondor.C
@@ -0,0 +1,139 @@
+#include "looper.h"
+#include "TChain.h"
+#include "TString.h"
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char **argv){
+  
+  //
+  // Input sanitation
+  //
+  if(argc<2){
+    cout<<" runBabyMaker takes five arguments: ./runBabyMakerCondor inputfile nevents file_number outpath samplelist" << endl;
+    cout<<" Need to provide at least outputfield infile, optional: nevents=-1"<<endl;
+    return 0;
+  }
+
+  TString infile(argv[1]);
+  int max_events = -1;
+  std::string outfileid("output");
+  std::string dirpath = ".";
+  if (argc > 2) max_events = atoi(argv[2]);
+  if (argc > 3) outfileid = argv[3];
+  if (argc > 4) dirpath = argv[4];
+  std::cout << "set max number of events to: " << max_events << std::endl;
+
+  bool isFastsim = bool(infile.Contains("FSPremix") || infile.Contains("FastAsympt25ns") || infile.Contains("Spring16Fast"));
+  bool isBadMiniAodV1 = bool(infile.Contains("V07-04-12_miniaodv1_FS"));
+  bool isDataFromFileName = bool(infile.Contains("run2_data"));
+  bool isSignalFromFileName = bool(infile.Contains("SMS"));
+  if(isDataFromFileName) cout << "running on DATA, based on file name: " << infile<<endl;
+  else if(isSignalFromFileName) cout << "running on SIGNAL, based on file name: " << infile<<endl;
+  else {
+    cout << "running on MC, based on file name: " << infile<<endl;
+  } 
+  //
+  // Initialize looper
+  //
+  babyMaker *mylooper = new babyMaker();
+
+  //
+  // Skim Parameters 
+  //
+  int nVtx              = 1;
+
+  float met             = 50;
+
+  int nGoodLeptons      = 0;
+  //int nGoodLeptons      = 1;
+  float goodLep_el_pt   = 20.0;
+  float goodLep_el_eta  = 1.4442;
+  float goodLep_mu_pt   = 20.0;
+  float goodLep_mu_eta  = 2.4;
+
+  float looseLep_el_pt  = 10.0;
+  float looseLep_el_eta = 2.4;
+  float looseLep_mu_pt  = 10.0;
+  float looseLep_mu_eta = 2.4;
+
+  float vetoLep_el_pt   = 5.0;
+  float vetoLep_el_eta  = 2.4;
+  float vetoLep_mu_pt   = 5.0;
+  float vetoLep_mu_eta  = 2.4;
+
+  int nJets             = 2;
+  float jet_pt          = 30.0;
+  float jet_eta         = 2.4;
+
+  int nBJets            = 0; 
+
+  bool applyJECfromFile = true;
+  int JES_central_up_down = 0;  //0 cetranl, 1 up, -1 down;
+  bool applyBtagSFs = true; 
+  bool applyLeptonSFs = true;
+  bool applyVetoLeptonSFs = true;
+  bool apply2ndlepVeto =  false;
+
+  float jet_ak8_pt      = 100.0;
+  float jet_ak8_eta     = 2.4;
+
+  int   nphs            = 0;
+  float phs_pt          = 20.0;
+  float phs_eta         = 2.4;
+
+  bool filltaus_  =  true;
+  bool filltracks_  =  true;
+  bool fillZll_  =  false;
+  bool fillPhoton_  =  false;
+  bool fillMETfilt_  =  false;
+  bool fill2ndlep_  =  false;
+  bool fillExtraEvtVar_  =  false;
+
+  bool fillAK4EF_  =  false;
+  bool fillAK4_Other_  =  false;
+  bool fillOverleps_  =  false;
+  bool fillAK4Synch_  =  false;
+  bool fillElID_  =  false;
+  bool fillIso_  =  false;
+  bool fillLepSynch_  =  false;
+  
+  // Input sanitation
+  if( !( goodLep_mu_pt>looseLep_mu_pt && looseLep_mu_pt>vetoLep_mu_pt) ){
+    cout << "   Problem with muon pT hierachy for good, loose, and veto pT!" << endl;
+    cout << "     Exiting..." << endl;
+    return 0;
+  }
+
+  if( !(goodLep_el_pt>looseLep_el_pt && looseLep_el_pt>vetoLep_el_pt) ){
+    cout << "   Problem with electron pT hierarchy for good, loose, and veto pT!" << endl;
+    cout << "     Exiting..." << endl;
+    return 0;
+  }
+
+
+  //
+  // Set Skim Variables
+  //
+  mylooper->setSkimVariables(isDataFromFileName,isSignalFromFileName,nVtx, met, nGoodLeptons, goodLep_el_pt,  goodLep_el_eta,  goodLep_mu_pt,  goodLep_mu_eta, looseLep_el_pt, looseLep_el_eta, looseLep_mu_pt, looseLep_mu_eta, vetoLep_el_pt, vetoLep_el_eta, vetoLep_mu_pt, vetoLep_mu_eta, apply2ndlepVeto,nJets, jet_pt, jet_eta, jet_ak8_pt, jet_ak8_eta, nBJets,  nphs, phs_pt, phs_eta, applyJECfromFile, JES_central_up_down, applyLeptonSFs, applyVetoLeptonSFs, applyBtagSFs, isFastsim, filltaus_,  filltracks_,  fillZll_,  fillPhoton_, fillMETfilt_,  fill2ndlep_,  fillExtraEvtVar_,  fillAK4EF_,  fillAK4_Other_,  fillOverleps_,  fillAK4Synch_,  fillElID_,  fillIso_,  fillLepSynch_);
+  //
+  // Intialize TChain, load samples
+  //
+  TChain *sample = new TChain("Events");
+  sample->Add(infile.Data());
+  if (sample->GetEntries() == 0) std::cout << "WARNING: no entries in chain. filename was: " << infile << std::endl;
+  //
+  // Run Looper
+  //
+  mylooper->looper(sample,outfileid,max_events);
+  //
+  // Return
+  //
+  return 0;
+}
