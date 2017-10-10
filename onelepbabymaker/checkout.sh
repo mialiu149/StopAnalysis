83835eb6137242ca2d015789c05ce37ca3fab15f
Mia Liu <miaoyuan.liu0@gmail.com>
2017-10-10 06:27:21 -0700

    add checkout script

---
+++ onelepbabymaker/checkout.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+setup_CMSSW_80X
+#dirname="WHAnalysis_2017dev"
+#git clone git@github.com:mialiu149/WHAnalysis.git ${dirname}
+#cd ${dirname}
+git clone git@github.com:cmstas/CORE.git
+cd CORE
+make -j 30
+cd ../onelepbabymaker/
+. setup.sh
